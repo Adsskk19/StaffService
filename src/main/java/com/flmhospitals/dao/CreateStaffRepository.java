f0f0ae01fd8c4f2dc85b7e65adae4c7e9d35bd99
anjaliganasala <anjaliganasala3@gmail.com>
2025-10-25 19:46:31 +0530

    added StaffService Repositories

---
+++ src/main/java/com/flmhospitals/dao/CreateStaffRepository.java
@@ -0,0 +1,9 @@
+package com.flmhospitals.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.flmhospitals.model.CreateStaff;
+
+public interface CreateStaffRepository extends JpaRepository<CreateStaff, Long>{
+
+}
