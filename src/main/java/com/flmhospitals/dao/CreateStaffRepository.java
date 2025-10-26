a50d653cbbdf1a40e95b6bf888ce149fe90705b0
anjaliganasala <anjaliganasala3@gmail.com>
2025-10-26 12:54:17 +0530

    deleted repositoy package

--- src/main/java/com/flmhospitals/dao/CreateStaffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flmhospitals.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.flmhospitals.model.CreateStaff;
-
-public interface CreateStaffRepository extends JpaRepository<CreateStaff, Long>{
-
-}
