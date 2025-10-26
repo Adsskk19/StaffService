ac85e60ee4c2dd7cbb3a6eded794f8dc6579cbdf
bhargavi-durga <bhargavi.durga2019@gmail.com>
2025-10-26 04:32:40 +0100

    Renamed the Package name

--- src/main/java/com/flmhospitals/utility/StaffIdGenerator.java
+++ src/main/java/com/flmhospitals/utility/StaffIdGenerator.java
@@ -1,7 +1,7 @@
-package com.flmhospitals.model.utility;
+package com.flmhospitals.utility;
 
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import com.flmhospitals.repository.StaffRepository;
 
 @Component
